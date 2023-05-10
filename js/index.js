f2d5650ce7c0f4feb350095c81d8ec1614964001
Juanp97 <juanpenaloza97@gmail.com>
2023-05-10 18:03:43 -0300

    Modificaciones varias

--- js/index.js
+++ js/index.js
@@ -1,22 +1,22 @@
 const estilos = document.documentElement.style;
 const carousel = document.querySelectorAll('.content');
+const imagenes = document.querySelectorAll('.content img')
 const puntos = document.querySelectorAll('.puntos li');
+const descripcion = document.querySelectorAll('.description')
 /* ------- Slider automatico cada cierto tiempo ------- */
 let contador = 0;
 
 const slider_automatico = () => {
     estilos.setProperty("--transicion", "all 1s ease")
     contador++;
-    if(contador === puntos.length){
-        contador=0;
+    if (contador === puntos.length) {
+        contador = 0;
         estilos.setProperty("--transicion", "none")
     }
     puntos.forEach(li => {
         li.classList.remove("li_activo");
     });
-
-    const traslacion = -100*contador;
-    console.log(traslacion)
+    const traslacion = -100 * contador;
     estilos.setProperty("--traslacion", traslacion + "%");
     puntos[contador].classList.add("li_activo");
 }
@@ -28,7 +28,7 @@ puntos.forEach((li, indice) => {
         puntos.forEach(li => {
             li.classList.remove("li_activo");
         })
-        const traslacion = -100*indice;
+        const traslacion = -100 * indice;
         estilos.setProperty("--traslacion", traslacion + "%")
         li.classList.add("li_activo");
         contador = indice;
@@ -36,4 +36,24 @@ puntos.forEach((li, indice) => {
         intervalo = setInterval(slider_automatico, 3000);
     });
 });
+/* Manejo del hover de la descripcion del slider para que funcione en dispositivos moviles */
+imagenes.forEach((img, i) => {
+    /* Cuando se presione en la imagen cambia su opacidad dependiendo de su valor actual */
+    img.addEventListener("mousedown", (e) => {
+        e.preventDefault();
+        const estilo = window.getComputedStyle(descripcion[i]);
+        console.log(estilo.getPropertyValue('opacity'))
 
+        Number(estilo.getPropertyValue('opacity')) > 0.5
+            ? descripcion[i].style.opacity = '0'
+            : descripcion[i].style.opacity = '1';
+    })
+    /* Hago que el evento hover se haga desde JS para evitar conflictos con los estilos en linea definidos por JS */
+
+    img.addEventListener("mouseover", () => {
+        descripcion[i].style.opacity = '1';
+    });
+    img.addEventListener("mouseout", () => {
+        descripcion[i].style.opacity = '0';
+    });
+})
